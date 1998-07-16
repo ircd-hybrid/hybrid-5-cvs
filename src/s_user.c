2f7776a86158e9f6a25add8645ad2798e0b1d11d
db <db>
1998-07-16 03:37:59 +0000

    fixed up botch in T oper flag handling remove all flag2 privs if oper deopers

--- src/s_user.c
+++ src/s_user.c
@@ -25,7 +25,7 @@
 static  char sccsid[] = "@(#)s_user.c	2.68 07 Nov 1993 (C) 1988 University of Oulu, \
 Computing Center and Jarkko Oikarinen";
 
-static char *rcs_version="$Id: s_user.c,v 1.62 1998/07/15 23:06:22 db Exp $";
+static char *rcs_version="$Id: s_user.c,v 1.63 1998/07/16 03:37:59 db Exp $";
 
 #endif
 
@@ -3578,7 +3578,7 @@ int	m_umode(aClient *cptr,
       Count.oper++;
     }
 
-  if ((setflags & FLAGS_NCHANGE) && !IsSetOpertcm(sptr))
+  if ((sptr->flags & FLAGS_NCHANGE) && !IsSetOpertcm(sptr))
     {
       sendto_one(sptr,":%s NOTICE %s :*** You need oper and T flag for +n",
 		 me.name,parv[0]);
@@ -3597,6 +3597,12 @@ int	m_umode(aClient *cptr,
 #endif
 	  delfrom_fdlist(sptr->fd, &oper_fdlist);
 
+
+	  sptr->flags2 &= ~(FLAGS2_OPER_GLOBAL_KILL|
+			    FLAGS2_OPER_REMOTE|
+			    FLAGS2_OPER_UNKLINE|
+			    FLAGS2_OPER_GLINE|
+			    FLAGS2_OPER_TCM);
 #ifdef USE_LINKLIST         
           while(cur_cptr)
             {
